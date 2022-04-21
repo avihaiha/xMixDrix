2a81bf0db3942501313a8c149e7514f4e36df39e
Avihai hadad <avihaihadad586@gmail.com>
2022-04-21 06:12:14 +0300

    add game borad

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,12 @@
 import './App.css';
+import Board from '../borad/Board';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          update <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lets play!</h1>
+        <Board />
       </header>
     </div>
   );
---
+++ src/components/borad/Board.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './Borad.css';
+
+function Board() {
+    let isXturn = true;
+    let numberOfClicks = 0;
+
+    function click(e){
+        if (numberOfClicks == 9){
+            return;
+        };
+
+        numberOfClicks++;
+        e.target.innerText = (isXturn)? "X":"O";
+        isXturn = !isXturn;
+    };
+
+    let cells = [];
+
+    for (var i = 0; i < 9; i++) {
+        cells.push(<div key={i} className="grid-cell" onClick={click}></div>);
+    }
+
+    return (
+        <div className="grid-container">
+            {cells}
+        </div>
+    )
+};
+
+
+
+export default Board;
